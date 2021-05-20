406951162bf91384e2f7f2e7b2fb289027d7acc6
Painketsu <mypainisname@gmail.com>
2021-05-20 20:28:26 +0200

    fix chapters without volume and multiple translation groups

--- DekaiMangaSorter.py
+++ DekaiMangaSorter.py
@@ -10,9 +10,12 @@ path = sys.argv[1]
 for ch in os.listdir(path):
   if not os.path.isdir(os.path.join(path, ch)):
     continue #not a dir
-
+  if ch[0:1] != 'V':
+    continue #no volume
+  
   chNoGroup = ch.partition(' [')[0]
   group = '[' + ch.partition('[')[2]
+
   volume,chapter = chNoGroup.split()
   volumePath = path + '/' + volume
   chapterPath = path + '/' + ch
@@ -21,11 +24,14 @@ for ch in os.listdir(path):
     os.makedirs(volumePath)
 
   for page in os.listdir(chapterPath):
-    newPageName = volume + ' ' + chapter.strip() + ' ' + page.strip()
+    newPageName = volume + ' ' + chapter + ' ' + page
     os.rename(os.path.join(chapterPath, page),
               os.path.join(chapterPath, newPageName))
     shutil.move(os.path.join(chapterPath, newPageName), volumePath)
   os.rmdir(chapterPath)
 
-#create a file with the TL group name
-open(path + "/" + group, 'a').close()
+  #create a file with the TL group name(s)
+  groupNameFile = open(path + "/" + "Translation Group(s).txt", 'a')
+  groupNameFile.write(chapter + ' by ' + group + '\n')
+  groupNameFile.close()
+    
